fabe00c1a406a7f9f419565314e4f23eb9a7662c
Max Carlquist <tenkaklet@gmail.com>
2020-11-25 23:25:08 +0100

    introduced project

---
+++ src/components/Pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <>
+         <h1>About</h1>
+         <p>This is the todo list app v.1.0.0 it is part of a tutorial</p>   
+        </>
+    )
+}
+
+export default About;
---
+++ src/components/Todos.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
+
+class Todos extends Component {
+  render() {
+    return this.props.todos.map((todo) => {
+        return <TodoItem markComplete={this.props.markComplete} delTodo={this.props.delTodo} todo={todo} key={todo.id} />
+    });
+  }
+}
+
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired,
+    markComplete: PropTypes.func.isRequired,
+    delTodo: PropTypes.func.isRequired
+}
+
+
+export default Todos;
